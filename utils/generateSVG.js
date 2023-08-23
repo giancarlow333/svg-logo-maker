19bdbe842ad3051605cc6406e2b48d3f70d34517
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-23 15:41:08 -0700

    Fix circle generation bug; add examples/

--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -6,7 +6,7 @@ function generateSVG (data) {
     // get the shape and the text
     if (data.logoShape == "circle") {
         logo = `<circle r="75" cx="150" cy="100" fill="${data.logoColor}" />`
-        text = `<text x="150" y="100" fill="${data.logoTextColor}" dominant-baseline="middle" text-anchor="central" font-family="Arial, Helvetica, sans-serif" font-weight="bold" font-size="3em">${data.logoText}</text>`;
+        text = `<text x="150" y="100" fill="${data.logoTextColor}" dominant-baseline="central" text-anchor="middle" font-family="Arial, Helvetica, sans-serif" font-weight="bold" font-size="3em">${data.logoText}</text>`;
     }
     else if (data.logoShape == "square") {
         logo = `<rect x="100" y="50" width="100" height="100" fill="${data.logoColor}" />`;
